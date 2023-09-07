38c46808dc8550cfede55466c1d93e9d2bd0c59e
Caio Monteiro <caiomonteiropro@gmail.com>
2023-09-07 10:17:08 -0300

    first commit

---
+++ DadosBancarios.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosBan
+{
+    internal class DadosBancarios
+    {
+        private string nome;
+        private int idade;
+        private string cpf;
+        private int agenciaBancaria;
+        private int contaBancaria;
+        private decimal saldoBancario; 
+
+        public DadosBancarios(string Nome, int Idade, string Cpf, int AgenciaBancaria, int ContaBancaria, decimal SaldoBancario)
+        {
+            this.nome = Nome;
+            this.idade = Idade;
+            this.cpf = Cpf;
+            this.agenciaBancaria = AgenciaBancaria;
+            this.contaBancaria = ContaBancaria;
+            this.saldoBancario = SaldoBancario;
+        }
+
+        public void setNome(string Newnome)
+        {
+            this.nome = Newnome;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setIdade(int Newidade)
+        {
+            this.idade= Newidade;
+        }
+        public int getIdade()
+        {
+            return this.idade; 
+        }
+
+        public void setCpf(string NewCpf)
+        {
+            this.cpf= NewCpf;   
+        }
+
+        public string getCpf()
+        {
+            return this.cpf;
+        }
+
+        public void setAgenciaBancaria(int NewAgBancaria)
+        {
+            this.agenciaBancaria = NewAgBancaria; 
+        }
+
+        public int getAgenciaBancaria()
+        {
+            return this.agenciaBancaria;
+        }
+
+        public void setContaBancaria(int NewConta)
+        {
+            this.contaBancaria = NewConta;
+        }
+
+        public int getContaBancaria()
+        {
+            return this.contaBancaria;  
+        }
+
+        public void setSaldoBancario(decimal NewSaldo)
+        {
+            this.saldoBancario = NewSaldo;
+        }
+        public decimal getSaldoBancario()
+        {
+            return this.saldoBancario; 
+        }
+
+        public void SaqueBancario(decimal valor)
+        {
+            if(valor <= saldoBancario)
+            {
+                saldoBancario -= valor;
+                Console.WriteLine("Saque realizado com sucesso, no valor de R$ " + valor +"!"); 
+            } else
+            {
+                Console.WriteLine("Operação não realizada, consulte o banco para verificar o que houve!"); 
+            }
+        }
+
+        public void DepositoBancario(decimal valor)
+        {
+            if(valor > 0)
+            {
+                saldoBancario += valor;
+                Console.WriteLine("Depósito realizado com sucesso, no valor de R$ " + valor + "!"); 
+            }
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("Seu saldo é: R$ " + saldoBancario+"!"); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DadosBan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosBancarios dados = new DadosBancarios("Carlos Alberto", 39,"465889689-78", 0304, 01012552-9, 12500);
+            dados.DepositoBancario(25254);
+            dados.ExibirSaldo();
+            dados.SaqueBancario(31200);
+            dados.ExibirSaldo();
+
+            dados.setNome("Marcos");
+            Console.WriteLine(dados.getNome());
+        }
+    }
+}
